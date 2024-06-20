09c804227bf331af24489a5adf73b81d0dc96bd9
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-06-20 12:47:50 -0400

    calling the actual API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import mitMediaLabLogo from "./assets/images/mit-media-lab-logo.png";
 import InputLabel from "@mui/material/InputLabel";
@@ -7,7 +7,38 @@ import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import styled from "styled-components";
-import CircularProgress from "@mui/material/CircularProgress";
+import axios from "axios";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+
+// Define the type for the input data
+interface DataPoint {
+  step: number;
+  value: number;
+}
+
+// Define the type for the output data
+interface SplitResult {
+  steps: number[];
+  values: number[];
+}
+
+interface ScalarData {
+  test_loss: DataPoint[];
+  test_acc: DataPoint[];
+  train_loss: DataPoint[];
+  train_acc: DataPoint[];
+}
+
+function splitStepAndValue(data: DataPoint[] | undefined): SplitResult {
+  if (!data) {
+    return { steps: [], values: [] };
+  }
+
+  const steps = data.map((item) => item.step);
+  const values = data.map((item) => item.value);
+  return { steps, values };
+}
 
 const Title = styled.h1``;
 const SubTitle = styled.p`
@@ -30,6 +61,7 @@ const Logo = styled.img`
 
 const ControlGroupContainer = styled.div`
   display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   justify-content: center;
 `;
@@ -38,68 +70,185 @@ const ResultChartContainer = styled.div`
   margin-top: 20px;
 `;
 
+const FormControlContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ChartsContainer = styled.div``;
+const ChartsSecondaryContainer = styled.div`
+  display: flex;
+  flex-directio: row;
+`;
+
+const MAX_NUM_OF_CLIENTS = 1; // FIXME: use 1 for now.
+const RETRY_WAIT_TIME_MS = 4000;
+
 const App = () => {
   const [hastrainingStarted, setHasTrainingStarted] = useState(false);
-  const [hasPastTrainingResult, setHasPastTrainingResult] = useState(false);
-  const [isResultReady, setIsResultReady] = useState(false);
+  const [isTrainingCompleted, setIsTrainingCompleted] = useState(false);
+  const [numOfClients, setNumOfClients] = useState(0);
   const [option, setOption] = useState("");
+  const [clientId, setClientId] = useState();
+  const [scalarData, setScalarData] = useState<ScalarData>({
+    test_loss: [],
+    test_acc: [],
+    train_loss: [],
+    train_acc: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `/api/experiment-plots/?user=${clientId}`
+        );
+
+        const responseSclarLength = Object.keys(
+          response?.data?.scalar_data
+        ).length;
+        const responseTestLossLength =
+          response?.data?.scalar_data?.test_loss?.length;
+        const currentTestLossLength = scalarData?.test_loss?.length;
+        if (response.data && responseSclarLength !== 0) {
+          // case 1: with data
+
+          // case 1.1: data < 100
+          // case 1.1.1: responseTestLossLength is the same as in the response, retry
+          // case 1.1.2: responseTestLossLength is different, setScalarData, retry
+
+          if (responseTestLossLength < 100) {
+            if (responseTestLossLength !== currentTestLossLength) {
+              setScalarData(response?.data?.scalar_data);
+            }
+
+            setTimeout(fetchData, RETRY_WAIT_TIME_MS);
+          } else {
+            setScalarData(response?.data?.scalar_data);
+            setIsTrainingCompleted(true);
+          }
+        } else {
+          // case: no data
+          console.log("Data is empty, retrying...");
+          setTimeout(fetchData, RETRY_WAIT_TIME_MS);
+        }
+      } catch (error) {
+        // case: error
+        console.error("Error fetching data:", error);
+        setTimeout(fetchData, RETRY_WAIT_TIME_MS); // Retry after RETRY_WAIT_TIME_MS seconds in case of error
+      }
+    };
+
+    if (clientId) {
+      fetchData();
+    }
+  }, [clientId]);
 
   const handleChange = (event: SelectChangeEvent) => {
     setOption(event.target.value);
   };
 
   const startTrainingHandler = () => {
+    if (numOfClients >= MAX_NUM_OF_CLIENTS) {
+      return;
+    }
+
+    setNumOfClients(numOfClients + 1);
     setHasTrainingStarted(true);
 
-    setTimeout(() => {
-      setIsResultReady(true);
-    }, 2000);
-  };
-
-  const stopTrainingHandler = () => {
-    setIsResultReady(false);
-    setHasPastTrainingResult(true);
-    setHasTrainingStarted(false);
+    axios
+      .get("/api/spawn-client/")
+      .then((response) => {
+        console.log("Training started:", response.data); // Log response data from the server
+        setClientId(response.data.client_id);
+      })
+      .catch((error) => {
+        console.error("Error starting training:", error);
+      });
   };
 
   const renderResultChart = () => {
-    if (hastrainingStarted) {
-      if (isResultReady) {
-        return (
+    const testLoss = splitStepAndValue(scalarData?.test_loss);
+    const testAcc = splitStepAndValue(scalarData?.test_acc);
+    const trainLoss = splitStepAndValue(scalarData?.train_loss);
+    const trainAcc = splitStepAndValue(scalarData?.train_acc);
+    return hastrainingStarted ? (
+      <ChartsContainer>
+        {isTrainingCompleted ? (
+          <Box sx={{ width: "100%" }}>
+            <b>
+              <i>Client 1</i>
+            </b>
+            : Training complete
+          </Box>
+        ) : (
+          <Box sx={{ width: "100%" }}>
+            <b>
+              <i>Client 1</i>
+            </b>
+            : Training in progress
+            <LinearProgress />
+          </Box>
+        )}
+
+        <ChartsSecondaryContainer>
           <LineChart
-            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            xAxis={[{ data: testLoss?.steps }]}
             series={[
               {
-                data: [2, 5.5, 2, 8.5, 1.5, 5],
+                data: testLoss?.values,
+                label: "Test Loss",
               },
             ]}
             width={500}
             height={300}
+            grid={{ vertical: true, horizontal: true }}
           />
-        );
-      } else {
-        return <CircularProgress />;
-      }
-    }
-
-    // check whether we have history data to show.
-    if (hasPastTrainingResult) {
-      return (
-        <LineChart
-          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-          series={[
-            {
-              data: [2, 5.5, 2, 8.5, 1.5, 5],
-            },
-          ]}
-          width={500}
-          height={300}
-        />
-      );
-    }
-
-    return null;
+          <LineChart
+            xAxis={[{ data: testAcc?.steps }]}
+            series={[
+              {
+                data: testAcc?.values,
+                label: "Test Accuracy",
+              },
+            ]}
+            width={500}
+            height={300}
+            grid={{ vertical: true, horizontal: true }}
+          />
+        </ChartsSecondaryContainer>
+        <ChartsSecondaryContainer>
+          <LineChart
+            xAxis={[{ data: trainLoss?.steps }]}
+            series={[
+              {
+                data: trainLoss?.values,
+                label: "Training Loss",
+              },
+            ]}
+            width={500}
+            height={300}
+            grid={{ vertical: true, horizontal: true }}
+          />
+          <LineChart
+            xAxis={[{ data: trainAcc?.steps }]}
+            series={[
+              {
+                data: trainAcc?.values,
+                label: "Training Accuracy",
+              },
+            ]}
+            width={500}
+            height={300}
+            grid={{ vertical: true, horizontal: true }}
+          />
+        </ChartsSecondaryContainer>
+      </ChartsContainer>
+    ) : null;
   };
+
+  const canPushSpawnButton = numOfClients < MAX_NUM_OF_CLIENTS;
 
   return (
     <Container>
@@ -113,7 +262,7 @@ const App = () => {
         </i>
       </SubTitle>
 
-      <div>
+      <FormControlContainer>
         <FormControl sx={{ m: 1, minWidth: 220 }} disabled>
           <InputLabel id="demo-simple-select-disabled-label">
             num_clients
@@ -155,27 +304,26 @@ const App = () => {
           ></Select>
           <FormHelperText>default: DARE</FormHelperText>
         </FormControl>
-      </div>
+      </FormControlContainer>
 
       {/* /////////////////////////////// */}
       <ControlGroupContainer>
-        {hastrainingStarted ? (
-          <Button
-            variant="outlined"
-            color="error"
-            onClick={stopTrainingHandler}
-          >
-            Stop training
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            color="success"
-            onClick={startTrainingHandler}
-          >
-            Start training
-          </Button>
-        )}
+        <Button
+          variant="contained"
+          color="success"
+          disabled={!canPushSpawnButton}
+          onClick={startTrainingHandler}
+        >
+          {canPushSpawnButton
+            ? "Spawn a new client"
+            : "you've reached the limit"}
+        </Button>
+        <SubTitle>
+          <i>
+            p.s. you can spawn up to {MAX_NUM_OF_CLIENTS}{" "}
+            {MAX_NUM_OF_CLIENTS === 1 ? "client" : "clients"} for the demo.
+          </i>
+        </SubTitle>
       </ControlGroupContainer>
 
       <ResultChartContainer>{renderResultChart()}</ResultChartContainer>
