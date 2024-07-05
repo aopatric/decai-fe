43aa0d29a1e82da11c8902b7b29a89188f2593f0
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-07-04 23:00:57 -0400

    attempt to fix the redirect issue

--- src/LogoutButton.tsx
+++ src/LogoutButton.tsx
@@ -5,6 +5,7 @@ import LogoutIcon from "@mui/icons-material/Logout";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
+  const returnToUrl = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URI;
 
   return (
     <Button
@@ -12,7 +13,7 @@ const LogoutButton = () => {
       variant="outlined"
       startIcon={<LogoutIcon />}
       sx={{ height: "40px", marginLeft: "20px" }}
-      onClick={() => logout()}
+      onClick={() => logout({ returnTo: returnToUrl } as LogoutOptions)}
     >
       Log Out
     </Button>
