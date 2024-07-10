dfabdecc1bcda04cff8f283901c2248b649c358f
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-07-10 01:31:23 -0400

    update logoug returnTo url

--- src/LogoutButton.tsx
+++ src/LogoutButton.tsx
@@ -5,14 +5,6 @@ import LogoutIcon from "@mui/icons-material/Logout";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
-  const returnToUrl = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URI;
-  console.log("returnToUrl", returnToUrl);
-  // Additional debug statement
-  if (!returnToUrl) {
-    console.error(
-      "REACT_APP_AUTH0_LOGOUT_REDIRECT_URI is not defined in the environment variables."
-    );
-  }
 
   return (
     <Button
@@ -21,7 +13,9 @@ const LogoutButton = () => {
       startIcon={<LogoutIcon />}
       sx={{ height: "40px", marginLeft: "20px" }}
       onClick={() =>
-        logout({ logoutParams: { returnTo: returnToUrl } } as LogoutOptions)
+        logout({
+          logoutParams: { returnTo: window.location.origin },
+        } as LogoutOptions)
       }
     >
       Log Out
