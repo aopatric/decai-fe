0fc7b43ceecf835923a4f49ee558be443140433c
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-07-10 01:21:53 -0400

    add the console log to test the returnToUrl

--- src/index.tsx
+++ src/index.tsx
@@ -9,17 +9,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-console.log("window.location.origin", window.location.origin);
-
 root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
-    authorizationParams={
-      {
-        // redirect_uri: window.location.origin,
-      }
-    }
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
   >
     <App />
   </Auth0Provider>
